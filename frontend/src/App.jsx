76fb61d10064a8afd464a209b1662b6b86a75363
Mehmet <mehmetgokce61@gmail.com>
2023-08-27 21:34:46 +0200

    router added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,44 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import React,{useContext} from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from "react-router-dom";
+import React, { useContext } from "react";
 import Register from "./components/Register";
 import Reservation from "./components/Reservation";
 import Login from "./components/Login";
 import User from "./components/User";
+import NotFound from "./components/NotFound";
 import { UserContext } from "./context/UserContext";
 const App = () => {
   const { isLoggedIn } = useContext(UserContext);
+
   return (
     <Router>
-      <Register />
-      <Login />
-      {isLoggedIn && <User />}
-      <Reservation />
+      <nav>
+        <NavLink to="register" activeClassName="active">
+          Register
+        </NavLink>
+        <NavLink to="login" activeClassName="active">
+          Login
+        </NavLink>
+        <NavLink to="user" activeClassName="active">
+          User
+        </NavLink>
+        <NavLink to="reservation" activeClassName="active">
+          Reservation
+        </NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/user" element={isLoggedIn && <User />} />
+        <Route path="/reservation" element={<Reservation />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </Router>
   );
 };
