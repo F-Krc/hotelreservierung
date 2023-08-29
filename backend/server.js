7bbef221c09616d37e9c2822ac6b3d3e4c0d2763
Fatih Karaca <fatih.karaca@gmx.net>
2023-08-29 11:53:20 +0200

    add backend url

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:4444', 'http://localhost:5173'],
+    origin: ['http://localhost:4444', 'http://localhost:5173', 'https://hotelreservierung.onrender.com'],
     credentials: true,
   })
 );
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,10 +7,10 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://hotelreservierung.onrender.com/',
         changeOrigin: true,
         secure: false,
       },
     },
   },
-})
+});
